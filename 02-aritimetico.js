f345638960f8704d85352ec0c1bd708037b8f73f
P Araujo <mxpedro.araujo11@gmail.com>
2024-02-07 11:26:06 -0300

    JavaScript- basico

---
+++ 02-aritimetico.js
@@ -0,0 +1,28 @@
+//utilizando operadores aritimeticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+//efetuar a soma de dois numeros informados por você (var nome_variavel = 12)
+
+var x = 12;
+var y = 4;
+
+console.log("a soma entre x e y é", y + x);
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y;
+
+console.log("soma =", soma);
+console.log("subtracacao =", subtracao);
+console.log("multiplicacao =", multiplicacao);
+console.log("divisão =", divisao);
+
+//regras para nomear indentificador (var, cont e let)
+//pode comecar com a-z, A-Z _ ou $
+
+var MinhaVariavel = 10;
+var outraVariavel = 20
+const _minhaconstante = 3.1514
+var $minhaVariavel = 11
